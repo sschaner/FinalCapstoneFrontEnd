2f6b68852a2d0023893b6f490c925daccc84be65
Steve Schaner <schanerst@gmail.com>
2022-04-05 14:31:59 -0400

    Links from trail search page go to a trail detail page,detail page.

--- src/app/trail/trail-detail/trail-detail.component.ts
+++ src/app/trail/trail-detail/trail-detail.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TrailRepositoryService } from '../trail-repository.service';
+import { WeatherRepositoryService } from '../weather-repository.service';
 
 @Component({
   selector: 'app-trail-detail',
   templateUrl: './trail-detail.component.html',
-  styleUrls: ['./trail-detail.component.css']
+  styleUrls: ['./trail-detail.component.css'],
 })
 export class TrailDetailComponent implements OnInit {
+  trailResult: any = [];
+  trailId: number = 1;
 
-  constructor() { }
+  constructor(
+    private trailService: TrailRepositoryService,
+    private weatherService: WeatherRepositoryService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.trailId = this.route.snapshot.params['trailId'];
+    this.trailResult = this.trailService
+      .getTrailById(this.trailId)
+      .subscribe((response) => {
+        this.trailResult = response;
+      });
   }
-
 }
