0d169df11c0af8d7fa4f10252d764e8309f47288
Zachary Tata <86603180+ztata@users.noreply.github.com>
2022-04-04 21:40:44 -0400

    v1 of trail search component
    added search function to trail search component to test trail and weather APIs

--- src/app/trail/trail-repository.service.ts
+++ src/app/trail/trail-repository.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrailRepositoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private apiUri = "https://localhost:44349/api/trail";
+
+  searchTrails(location: string){
+    console.log("called search trails api")
+    return this.http.get(`${this.apiUri}/?searchTerm=${location}`);
+  }
 }
--- src/app/trail/trail-search/trail-search.component.ts
+++ src/app/trail/trail-search/trail-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { TrailRepositoryService } from '../trail-repository.service';
+import { WeatherRepositoryService } from '../weather-repository.service';
 
 @Component({
   selector: 'app-trail-search',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrailSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private trailService: TrailRepositoryService, private weatherService: WeatherRepositoryService) { }
 
   ngOnInit(): void {
+  }
+
+  trailResult: any = [];
+  weatherResult: any = [];
+
+  searchTrails(form: NgForm){
+    console.log("called search trails method")
+   
+    let location: string = form.form.value.location;
+    this.trailResult = this.trailService.searchTrails(location).subscribe(
+      (response) => {this.trailResult = (response)}
+    );
+    console.log(this.trailResult)
+
+    this.weatherResult = this.weatherService.getCurrentWeather(location).subscribe(
+      (response) => {this.weatherResult = (response)}
+    );
+    console.log(this.weatherResult)
+
   }
 
 }
