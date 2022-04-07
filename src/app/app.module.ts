c6583c09ddcefda71fa7d5947106404b80804aac
Zachary Tata <86603180+ztata@users.noreply.github.com>
2022-04-06 20:14:20 -0400

    added expansion panels to trail search
    added expansion panels to trail search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +12,7 @@ import { TrailModule } from './trail/trail.module';
 import { UserModule } from './user/user.module';
 import { HomeModule } from './home/home.module';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,7 +25,9 @@ import { FormsModule } from '@angular/forms';
     HomeModule,
     TrailModule,
     UserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/trail/trail.module.ts
+++ src/app/trail/trail.module.ts
@@ -4,10 +4,11 @@ import { TrailSearchComponent } from './trail-search/trail-search.component';
 import { TrailDetailComponent } from './trail-detail/trail-detail.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [TrailSearchComponent, TrailDetailComponent],
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, MatExpansionModule],
   exports: [TrailSearchComponent, TrailDetailComponent],
 })
 export class TrailModule {}
