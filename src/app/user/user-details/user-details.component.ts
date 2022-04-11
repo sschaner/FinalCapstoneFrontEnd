2e9a6a62c4121f6cebb6bc241ec207b01871e1db
Zachary Tata <86603180+ztata@users.noreply.github.com>
2022-04-10 22:00:00 -0400

    added user details page

---
+++ src/app/user/user-details/user-details.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { UserRepositoryService } from '../user-repository.service';
+import {
+  faCircleInfo,
+  faHeartCirclePlus,
+  faHeartCircleMinus,
+} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent implements OnInit {
+
+  constructor(private userService: UserRepositoryService) { }
+  currentUser: any;
+  userFavorites: any = [];
+  faCircleInfo = faCircleInfo;
+  faHeartCircleMinus = faHeartCircleMinus;
+
+  ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe((value) => {
+      this.currentUser = value;
+    });    
+  }
+
+  removeTrailFromFavorites(id: number){
+      
+  }
+
+}
