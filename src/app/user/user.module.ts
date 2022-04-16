6665e9a70b894fbf1f09532920bcab973431df3b
Zachary Tata <86603180+ztata@users.noreply.github.com>
2022-04-16 13:55:46 -0400

    made register into a reactive form and added input validation

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRegisterComponent } from './user-register/user-register.component';
-import { FormControl, FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -17,7 +19,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
      FormsModule, 
      MatExpansionModule, 
      RouterModule,
-     FontAwesomeModule
+     FontAwesomeModule,
+     ReactiveFormsModule,
+     FormsModule,
+     RxReactiveFormsModule,
+     NgbAlertModule
     ],
   exports: [UserRegisterComponent],
 })
