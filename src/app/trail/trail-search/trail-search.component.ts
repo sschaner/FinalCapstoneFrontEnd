dccc1c8b82e53f59d81b7d62e54ecc368ab4f109
Steve Schaner <schanerst@gmail.com>
2022-05-18 08:17:17 -0400

    Chart uses data to draw chart.

--- src/app/trail/trail-search/trail-search.component.ts
+++ src/app/trail/trail-search/trail-search.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { TrailRepositoryService } from '../trail-repository.service';
 import { WeatherRepositoryService } from '../weather-repository.service';
+import { Chart, registerables } from 'chart.js';
 
-import { faCircleInfo, faHeartCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleInfo,
+  faHeartCirclePlus,
+} from '@fortawesome/free-solid-svg-icons';
 import { UserRepositoryService } from 'src/app/user/user-repository.service';
 
 @Component({
@@ -14,6 +18,12 @@ import { UserRepositoryService } from 'src/app/user/user-repository.service';
 export class TrailSearchComponent implements OnInit {
   trailResult: any = [];
   weatherResult: any = [];
+  trailLengths: any = [];
+  trailRatings: any = [];
+  trailNames: any = [];
+  trailBarChart: any;
+  trailBarChartCanvas: any;
+  trailBarChartVisibility: boolean = false;
   faCircleInfo = faCircleInfo;
   faHeartCirclePlus = faHeartCirclePlus;
   currentUser: any;
@@ -24,12 +34,14 @@ export class TrailSearchComponent implements OnInit {
     private trailService: TrailRepositoryService,
     private weatherService: WeatherRepositoryService,
     private userService: UserRepositoryService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.userService.getCurrentUser().subscribe((value) => {
       this.currentUser = value;
     });
+    this.trailBarChart = document.querySelector('#trail-length-bar-chart');
+    Chart.register(...registerables);
   }
 
   ngAfterViewInit() {
@@ -41,12 +53,27 @@ export class TrailSearchComponent implements OnInit {
   }
 
   searchTrails(form: NgForm) {
+    if (this.trailBarChartCanvas != null) {
+      this.trailBarChartCanvas.destroy();
+    }
     let location: string = form.form.value.location;
-    this.trailResult = this.trailService
-      .searchTrails(location)
-      .subscribe((response) => {
+    this.trailResult = this.trailService.searchTrails(location).subscribe(
+      (response) => {
         this.trailResult = response;
-      }, err => { this.trailResult = [], this.failureMessage = 'Sorry, it does not look like we have any info on trails in this area! Try another city or postal code.' });
+        for (var i = 0; i < this.trailResult.length; i++) {
+          this.trailLengths.push(parseFloat(this.trailResult[i].length));
+          this.trailRatings.push(parseFloat(this.trailResult[i].rating));
+          this.trailNames.push(this.trailResult[i].name);
+        }
+
+        this.loadTrailBarChart();
+      },
+      (err) => {
+        (this.trailResult = []),
+          (this.failureMessage =
+            'Sorry, it does not look like we have any info on trails in this area! Try another city or postal code.');
+      }
+    );
 
     this.weatherResult = this.weatherService
       .getCurrentWeather(location)
@@ -60,5 +87,56 @@ export class TrailSearchComponent implements OnInit {
     this.userService.addFavoriteTrail(id, userId).subscribe();
     this.successMessage =
       "Success! You can view all your favorited trails on the 'Favorites' page.";
+  }
+
+  loadTrailBarChart() {
+    this.trailBarChartVisibility = true;
+    this.trailBarChartCanvas = new Chart(this.trailBarChart, {
+      type: 'bar',
+      data: {
+        labels: this.trailNames,
+        datasets: [
+          {
+            label: 'Length',
+            data: this.trailLengths,
+            backgroundColor: ['red'],
+            borderColor: ['black'],
+            borderWidth: 1,
+          },
+          {
+            label: 'Difficulty (Out of 5)',
+            data: this.trailRatings,
+            backgroundColor: ['blue'],
+            borderColor: ['black'],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            max: Math.max(...this.trailLengths) + 5,
+          },
+          x: {
+            ticks: {
+              autoSkip: false,
+              maxRotation: 45,
+              minRotation: 45,
+            },
+            title: {
+              display: true,
+              text: 'Trail Name',
+            },
+          },
+        },
+        plugins: {
+          title: {
+            display: true,
+            text: 'Trails',
+          },
+        },
+      },
+    });
   }
 }
